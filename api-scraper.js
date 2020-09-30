290d908c4ec9ded286ff6022a3bfecb3bef2d16e
Westh <haakonwesth@gmail.com>
2020-09-30 15:33:28 +0200

    perf: ⚡️ make api-scraper faster by dropping batching

--- api-scraper.js
+++ api-scraper.js
@@ -1,10 +1,8 @@
 const { Observable } = require('rxjs')
 const fetch = require('node-fetch')
-const postalCodes = require('./postal-codes')
 
 const METHOD = 'API'
-const BATCH_SIZE = 10
-const LIMIT = 10000
+const LIMIT = 100000
 
 module.exports = ({
   initFile,
@@ -14,24 +12,19 @@ module.exports = ({
   MAST_DATABASE_BASE_URL
 }) => {
   return new Observable(async observer => {
-    observer.next('Starting...')
+    observer.next('Scraping started...')
 
     for (let i = 0; i < technologiesToScrape.length; i++) {
       initFile(technologiesToScrape[i].name, METHOD)
 
-      for (let j = 0; j < postalCodes.length; j += BATCH_SIZE) {
-        const batch = postalCodes.slice(j, j + BATCH_SIZE)
+      observer.next(`Scraping ${technologiesToScrape[i].name} all at once`)
 
-        observer.next(`Scraping ${technologiesToScrape[i].name} postal codes between ${batch[0]}-${batch[batch.length - 1]}`)
+      const result = await fetch(
+          `${MAST_DATABASE_BASE_URL}/Master/antenner.json?` +
+          `teknologi=${technologiesToScrape[i].value}&maxantal=${LIMIT}`
+      ).then(response => response.json())
 
-        const results = await Promise.all(batch.map(postalCode => {
-          return fetch(
-              `${MAST_DATABASE_BASE_URL}.json?postnr=${postalCode}&teknologi=${technologiesToScrape[i].value}&maxantal=${LIMIT}`
-          ).then(response => response.json())
-        }))
-
-        writeDataToFile(results.flat(), technologiesToScrape[i].name, METHOD)
-      }
+      writeDataToFile(result, technologiesToScrape[i].name, METHOD)
 
       endFile(technologiesToScrape[i].name, METHOD)
     }
