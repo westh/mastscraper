f5e37d52960ba88980cf83e209ba66deb085eeb0
Westh <haakonwesth@gmail.com>
2020-10-04 14:20:35 +0200

    feat: 🎸 added translation of data coming from the API

--- api-scraper.js
+++ api-scraper.js
@@ -4,6 +4,39 @@ const fetch = require('node-fetch')
 const METHOD = 'API'
 const LIMIT = 100000
 
+function serviceTypeTranslation (serviceType) {
+  if (serviceType === 'Andre') return 'other'
+  if (serviceType === 'Mobiltelefoni') return 'mobile communication'
+  if (serviceType === 'Teknologineutrale tilladelser') return 'neutral'
+  return 'unknown'
+}
+
+function danishToEnglishMapping (item) {
+  return {
+    address: {
+      city: item?.postnummer?.navn,
+      postalCode: item?.postnummer?.nr,
+      street: item?.vejnavn?.navn,
+      number: item?.husnr
+    },
+    dateOfCommissioning: item?.idriftsaettelsesdato,
+    plannedDateOfCommissioning: item?.forventet_idriftsaettelsesdato,
+    etrs89Coordinates: {
+      east: item?.etrs89koordinat?.oest,
+      north: item?.etrs89koordinat?.nord
+    },
+    wgs84Coordinates: {
+      latitude: item?.wgs84koordinat?.bredde,
+      longitude: item?.wgs84koordinat?.laengde
+    },
+    serviceType: serviceTypeTranslation(item?.tjenesteart?.navn),
+    technology: item?.teknologi,
+    frequency: item?.frekvensbaand,
+    radius: item?.radius_i_meter,
+    uniqueStationName: item?.unik_station_navn
+  }
+}
+
 module.exports = ({
   initFile,
   writeDataToFile,
@@ -24,7 +57,7 @@ module.exports = ({
           `teknologi=${technologiesToScrape[i].value}&maxantal=${LIMIT}`
       ).then(response => response.json())
 
-      writeDataToFile(result, technologiesToScrape[i].name, METHOD)
+      writeDataToFile(result.map(mast => danishToEnglishMapping(mast)), technologiesToScrape[i].name, METHOD)
 
       endFile(technologiesToScrape[i].name, METHOD)
     }
